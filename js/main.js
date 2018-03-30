96cd5791ad480fd8d298e62536b4126e2cebbb78
Andrea <andrea.wayte@gmail.com>
2018-03-30 15:50:06 -0700

    update project descriptions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 var portfolioInfo = {
-    'calculator': {
-        'title': 'Calculator',
-        'description': '<p>Designed and developed a basic calculator to compute basic calculations using order of operations. The user input displays on the screen and computes the input when the equal sign is clicked. Each button creates an object which is filtered before calculating. The filtered input then uses objects to calculate the order of operations then uses function for the different operands. I use operands as keys to perform basic subtraction, addition, multiplication and division. </p>',
-        'technologies': 'HTML, CSS, Javascript, JQuery',
-        'liveSiteUrl': 'http://calculator.andreawayte.com',
-        'githubUrl': 'https://github.com/andreasandpiper/calculator-javascript',
-        'imageSrc': 'img/images/calculator.jpg'
-    },
     'memoryMatch': {
         'title': 'Memory Match',
-        'description': '<p>Created a memory match game that adds a twist to the normal game. The player must find all the element matches before losing all three lives. However, the game board can change based on finding molecule combinations. For example, Hydrogen and Chlorine create hydrochloric acid, which causes the player to loose a life. The game incorporate animated effects, countdowns, and board shuffles. </p><p>I was inspired to create a science game based on my background of being a science teacher. The periodic table is iconic to science, and I wanted to create a game where people could learn some information about elements and molecules by playing a fun game. </p>',
+        'short_description': "An elemental version of the classic, memory match game.",
+        'tech_description': "The game is written in Javascript, with JQuery events and DOM manipulation. I used Object Oriented Programming to split up the functionality of the different parts of the game. The game object handles the logic such as telling the card object when to flip over, when the Cow object needs to produce methane gas, and the game stats when to add, subtract, or use a resource. It also instructs the Curium countdown object to start counting down or make the cow neon. Since there are many complicated parts to the game, creating seperate objects helped to keep the code organized and readable. I used Flask to serve the different pages, and plan to incorporate a leaderboard model in the next version.",
+        'other_description': "I wanted to create a game in which the user passively learns about elements of the periodic table. The player must find all the element matches before losing all three lives. However, the game board can change based on finding molecule combinations. For example, Hydrogen and Chlorine create hydrochloric acid, which causes the player to loose a life. The game incorporate animated effects, countdowns, and board shuffles. I was inspired to create a science game based on my background of being a science teacher. The periodic table is iconic to science, and I wanted to create a game where people could learn some information about elements and molecules by playing a fun game.", 
         'technologies': 'Python, Flask, Heroku, Jinja2, HTML, CSS, JavaScript, jQuery, Git',
         'liveSiteUrl': 'https://memorymatch-flask.herokuapp.com/',
         'githubUrl': 'https://github.com/andreasandpiper/flask-memorymatch',
@@ -17,7 +11,9 @@ var portfolioInfo = {
     },
     'hawkandheron': {
         'title': 'The Hawk and Heron',
-        'description': '<p>The Hawk and Heron was inspired by my need to create a simple website to display my crafts. I was previously using Wordpress and could not find a theme I thought displayed my crafts well. I then began learning HTML and CSS and created my own custom website. Since I was creating a static website, I used Jekyll to generate my site. Jekyll incorporates the Liquid render to easily loop through a list of images and render them to the page using the same format. </p><p>The focus of the website is to display images of products and provide links to social media pages. I use this website to help customers see the variety of products I make and guide them to my Etsy shop. I am currently using this website to begin practicing Bootstrap4. </p>',
+        'short_description': "A user-friendly website to display a collection of handmade crafts.",
+        'tech_description': "The website uses HTML and CSS to style the page and elements while using bootstrap to create responsive containers. I used Jekyll site generator as the framework for the project. Jekyll incorporates the Liquid render to easily loop through a list of image and render them to the page using the same format. I create reusables templates for the header and footer for each page. ",
+        'other_description': "The Hawk and Heron was inspired by my need to create a simple website to display my crafts. The focus of the website is to display images of products and provide links to social media pages.",
         'technologies': 'HTML, CSS, Bootstrap, Jekyll, Liquid templating, GitHub Pages',
         'liveSiteUrl': 'http://thehawkandheron.com',
         'githubUrl': 'https://github.com/andreasandpiper/andreasandpiper.github.io',
@@ -25,7 +21,9 @@ var portfolioInfo = {
     },
     'sbbikepark': {
         'title': 'Santa Barbara Bike Park',
-        'description': '<p>Mountain biking is a big part of my life, and I am involved with the local group in Santa Barbara, updating their website and email and social media marketing. One of the projects was to build a new website to promote the building of pumptracks in the community. I used Wordpress as the CMS, Content Management System so others could also contribute text to the website. </p><p>I incorporated a child theme to customize the CSS and add additional features to the homepage. I added the static background and sponsors bar using a php file. I currently maintain the website by updating content when needed and performing backups of the website. ',
+        'short_description': "An informative Wordpress website about an upcoming project in Santa Barbara",
+        'tech_description': "I used Wordpress as the CMS, Content Management System so others could also contribute content to the website. I incorporated a child theme to customize the CSS and add additional features to the homepage. I added the static background and sponsors bar using a php file. I currently maintain the website by updating content when needed and performing backups of the website.",
+        'other_description': "Mountain biking is a big part of my life, and I am involved with the local group in Santa Barbara, updating their website and email and social media marketing. One of the projects was to build a new website to promote the building of pumptracks in the community. ",
         'technologies': 'CSS, HTML, Wordpress, sFTP',
         'liveSiteUrl': 'https://www.sbbikepark.com',
         'githubUrl': '',
@@ -33,7 +31,9 @@ var portfolioInfo = {
     },
     'beetsandeats': {
         'title': 'Beets & Eats',
-        'description': '<p>Beets and Eats is an application to help users plan an evening out on the town. To use the application, users enter a location and date to populate a visual list of the events happening on that date. Links to the events and Ticketmaster are available for each event.  Once an event is chosen, the map will populate many nearby food and drink locations. When a location a click, more information including links are provided. </p><p> My contribution to this 2-day Hackathon project was using information from Ticketmaster and Yelp to populate a Google map with nearby locations. I used closures to populate the sidebar with more information once the user clicked an icon as well as displaying basic information in a label window.  </p><p>Beets and Eats uses data from GoogleMaps, Ticketmaster and Yelp to populate a map with restaurants and bars surrounding a desired event venue. </p>',
+        'short_description': "Plan a night on the town, without having to walk too far!",
+        'tech_description': "Beets and Eats uses data from GoogleMaps, Ticketmaster and Yelp to populate a map with restaurants and bars surrounding a desired event venue. We use AJAX calls and supply specific data for our request, such as a zip code for a location. Users enter a location and date in which we request information from Ticketmaster to populate a list of all the events occuring on the specified date. With the use of closures, when users click on the event, the website scrolls automatically to the map, which becomes populated with nearby restaurants and lounges from the Yelp API. Users can click on the icons to learn more about each location. ",
+        'other_description': "The project was accomplished during a 2 day Hackathon. The initial preparation was creating quick wireframes and a SCRUM list of all the desired features. We created a repository on GitHub, and included a Javascript file incorporating JSDocs to specify the location and description for each function to be written. Once set up, we delegated tasks and a sprint using Meistertask. We Git as version control, and worked off of a development branch, with a separate branch for each feature. By using these task managing systems, we were able to produce a functioning and appealing web application!",
         'technologies': 'HTML, CSS, JQuery, AJAX, Google Maps API, Yelp API, Ticketmaster API, Bootstrap',
         'liveSiteUrl': 'http://beatsandeats.andreawayte.com',
         'githubUrl': 'https://github.com/andreasandpiper/c12.17Hackathon2',
@@ -41,7 +41,9 @@ var portfolioInfo = {
     },
     'closeyourtabs': {
         'title': 'Close Your Tabs',
-        'description': '<p>Close Your Tabs incorporates a Chrome extension to keep track of how long a tab has been inactive and active. We display your current tabs from all windows and color them according to the amount of time elapsed since the tab was used. This tools helps you see which tabs are not being used in a colorful manner.</p><p> My contribution to this program was developing the Chrome Extension and wrote the FAQ component in React. I built the extension using an Agile approach to complete a basic prototype to then focus on each next feature. The extension uses Chrome Platform APIs to listen for events within the browser, such as creating a new tab, moving a tab, deleting a tab, etc. I weaved the APIs together to create a system which keeps track of all the current tabs and the time on inactivation. </p><p>The web application is an external view of the extension, giving the user more features to manage their tabs. The extension runs regardless of the user being logged in. A person can sign up using Google Oauth which changes the user’s state in the extension and incorporates http requests to the server so the webpage can display the user’s tab information.</p>',
+        'short_description': "A Chrome extension to keep tabs on your tabs.",
+        'tech_description': "Close Your Tabs incorporates a React website, Node.js Javascript environment with mySQL database, and a Chrome extension. The three components join together to create a system to keep track of the current tabs in a Chrome browser session and access those tabs on a different browser or device. The extension uses Chrome Platform APIs to listen for events within the browser, such as creating a new tab, moving a tab, deleting a tab, etc. The extension popup displays the tabs and color codes them according to the time elapsed since last used. We used Google Oauth and passport for user authentication and cookie sessions. The extension is able to know if a user is logged in by injecting content script into the website to listen for the users actions with the website. This also enables injecting scripts to allow the user to manipulate their tabs from within the website as well. The extension communicates with the remote server using XHR requests.",
+        'other_description': "The idea for Close Your Tabs spawned from observing developers with “mountains” of tabs, which titles can become difficult to read when searching for a particular website open. My contribution was developing the extension and writing the FAQ React component as well as UI/UX design for the project. We used Agile methodologies to create an increasingly more complex product. Meistertask was the centrally hub for delegating tasks, reporting bugs, and keeping track of excel sheets and API documentation we created. Git was used as version control.",
         'technologies': 'Javascript, ReactJS, Chrome Extension, Chrome Platform API, XMLHTTPRequests, HTML5, CSS3, Node, MySQL, Google OAuth, Axios ',
         'liveSiteUrl': 'http://www.closeyourtabs.com',
         'githubUrl': 'https://github.com/andreasandpiper/closeyourtabs-chrome-extension',
@@ -110,18 +112,24 @@ $(document).ready(function () {
 
 function showProjectInfo() {
     var projectInfo = $(this).closest('.portfolio-div').attr('data-title');
-    var projectTitle = portfolioInfo[projectInfo].title;
-    var projectDescript = portfolioInfo[projectInfo].description;
-    var projectImage = portfolioInfo[projectInfo].imageSrc;
-    var projectTech = portfolioInfo[projectInfo].technologies;
-    var liveLink = portfolioInfo[projectInfo].liveSiteUrl;
-    var gitHubLink = portfolioInfo[projectInfo].githubUrl;
+    var project =  portfolioInfo[projectInfo]; 
+    var projectTitle = project.title;
+    var projectImage = project.imageSrc;
+    var projectTech = project.technologies;
+    var liveLink = project.liveSiteUrl;
+    var gitHubLink = project.githubUrl;
+    var general_desc = project.short_description;
+    var tech_desc = project.tech_description;
+    var other_desc = project.other_description;
     $("#portfolioModal").find('.modal-title').text(projectTitle);
-    $("#portfolioModal").find('.modal-body .description').html(projectDescript);
     $("#portfolioModal").find('.modal-body img').attr('src', projectImage);
     $("#portfolioModal").find('.modal-body span').html(projectTech);
     $("#portfolioModal").find('.modal-body .live').attr('href', liveLink);
     $("#portfolioModal").find('.modal-body .github').attr('href', gitHubLink);
+    $("#portfolioModal").find('.modal-subtitle').text(general_desc);
+    $("#portfolioModal").find('div.description > p:nth-child(2)').text(tech_desc);
+    $("#portfolioModal").find('.modal-body .description p:last-child').text(other_desc);
+
     if (gitHubLink === '') {
         $("#portfolioModal").find('.modal-body .github').hide();
     } else {
